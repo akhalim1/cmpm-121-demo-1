12d6ca48df380ffae672014221ed8325c95771ff
Alexander Halim <akhalim@ucsc.edu>
2024-10-08 11:59:43 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,18 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter: number = 0;
+
+const counterDiv = document.createElement("div");
+counterDiv.textContent = `${counter} swords`;
+app.append(counterDiv);
+
 const button = document.createElement("button");
 button.textContent = "⚔️ Click";
 button.addEventListener("click", () => {
-  console.log("Clicked");
+  // console.log("Clicked");
+  counter += 1;
+  counterDiv.textContent = `${counter} swords`;
 });
 
 app.append(button);
